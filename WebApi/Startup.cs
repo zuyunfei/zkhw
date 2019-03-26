9a7a8cfc613c30717e7feae8cda94bf6dd300cdf
clark <ben@langben.com>
2019-03-26 17:39:18 +0800

    api

---
+++ WebApi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApi.Startup))]
+namespace WebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
